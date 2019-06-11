a10fc6845ecca6a97678d63cce3f3497022b1e0b
mariosky <mariosky@gmail.com>
2019-06-10 17:22:12 -0700

    process redis logs for coco

---
+++ coco.py
@@ -0,0 +1,89 @@
+import numpy as np
+import ga_worker.bbobbenchmarks as bn
+import random
+
+class CoCoData(object):
+    def __init__(self, dim, function, instance, nbptsevals= 20, nbptsf = 5):
+        self.evalsTrigger = 1
+        self.function = function
+        self.instance = instance
+        self.lasteval_num = 0
+        self.fTrigger = np.inf
+        self.idxFTrigger = np.inf
+        self.nbptsf = nbptsf
+        self.idxEvalsTrigger = 0
+        self.nbptsevals = nbptsevals
+        self.dim = dim
+        self.idxDIMEvalsTrigger = 0.
+        self.nbFirstEvalsToAlwaysWrite = 1
+
+
+    def evalfun(self, algorithm, gen, ngen, fmin, fopt, error, sol,buffr=None,hbuffr=None):
+        fmin = float(fmin)
+        fopt = float(fopt)
+
+        error = float(error)
+
+        self.lasteval_num = self.lasteval_num + int(ngen)
+
+        if (self.lasteval_num >= self.evalsTrigger or fmin  - fopt < self.fTrigger):
+            #We must write if we are past the trigger?
+
+            if self.lasteval_num >= self.evalsTrigger:
+                #In order to pass an assertion in DataSet() we add a fake first eval using a random solution
+                if not buffr:
+                    #buffr.append(self.sprintData(1, algorithm, gen, ngen, fmin, fopt, error, sol))
+                    random_sol =  [10. * random.random() - 5 for _ in range(self.dim)]
+                    function = bn.dictbbob[self.function](self.instance)
+                    fval = function(random_sol)
+                    fopt = function.getfopt()
+                    buffr.append(self.sprintData(1, algorithm, gen, ngen, fval, fopt, fopt - fval, random_sol))
+
+                buffr.append(self.sprintData(self.lasteval_num, algorithm, gen, ngen, fmin, fopt, error, sol))
+
+                while self.lasteval_num >= np.floor(10 ** (self.idxEvalsTrigger / self.nbptsevals)):
+                    self.idxEvalsTrigger += 1
+                while self.lasteval_num >= self.dim * 10 ** self.idxDIMEvalsTrigger:
+                    self.idxDIMEvalsTrigger += 1
+                self.evalsTrigger = min(np.floor(10 ** (self.idxEvalsTrigger / self.nbptsevals)),
+                                        self.dim * 10 ** self.idxDIMEvalsTrigger)
+                if self.lasteval_num < self.nbFirstEvalsToAlwaysWrite:
+                    self.evalsTrigger = self.lasteval_num + 1
+
+            # Also if we have a better solution
+            if fmin - fopt < self.fTrigger:  # minimization only
+                if not hbuffr:
+                    random_sol = [10. * random.random() - 5 for _ in range(self.dim)]
+                    function = bn.dictbbob[self.function](self.instance)
+                    fval = function(random_sol)
+                    fopt = function.getfopt()
+                    hbuffr.append(self.sprintData(1, algorithm, gen, ngen, fval, fopt, fopt - fval, random_sol))
+
+                    #hbuffr.append(self.sprintData(1, algorithm, gen, ngen, fmin, fopt, error, sol))
+                hbuffr.append(self.sprintData(self.lasteval_num, algorithm, gen, ngen, fmin, fopt, error, sol))
+                if fmin <= fopt:
+                    self.fTrigger = -np.inf
+                else:
+                    if np.isinf(self.idxFTrigger):
+                        self.idxFTrigger = np.ceil(np.log10(fmin - fopt)) * self.nbptsf
+                    while fmin - fopt <= 10 ** (self.idxFTrigger / self.nbptsf):
+                        self.idxFTrigger -= 1
+                    self.fTrigger = min(self.fTrigger, 10 ** (self.idxFTrigger / self.nbptsf))  # TODO: why?
+
+
+
+    def sprintData(self, lasteval_num, algorithm, gen, ngen, fmin, fopt, error, sol):
+        """Format data for printing."""
+
+        res = ('%d %+10.9e %+10.9e %+10.9e %+10.9e'
+               % (lasteval_num, fmin - fopt,
+                  fmin - fopt, fmin,
+                  fopt))
+
+        if len(sol) < 22:
+            tmp = []
+            for i in sol:
+                tmp.append(' %+5.4e' % i)
+            res += ''.join(tmp)
+        res += '\n'
+        return res
---
+++ coco_process_logs.py
@@ -0,0 +1,130 @@
+
+import urllib.parse, ast
+import os, redis, json
+from coco import CoCoData
+from itertools import groupby
+from operator import itemgetter
+ 
+
+def process_logs(experiment_id, list_name, redis_host = 'localhost', redis_port = 6379):
+    r = redis.Redis(host=redis_host, port=redis_port)
+
+    DATA_FOLDER = './experiment_data/' + str(experiment_id) + '/'
+    experiment = 'swarm_ea' +':' + str(experiment_id)
+    data = [json.loads(i) for i in r.lrange( list_name, 0, -1) if json.loads(i)['experiment_id'] == '1234']
+    #data = [json.loads(i) for i in r.lrange(experiment, 0, -1)]
+    data.reverse()
+
+    #IF not exisits
+    try:
+        os.makedirs(DATA_FOLDER)
+    except OSError:
+        pass
+    with open(DATA_FOLDER+experiment+'.json', 'w') as f:
+        json.dump(data, f)
+
+
+    grp_benchmark = itemgetter("benchmark","dim")
+    grp_instance = itemgetter("benchmark","instance")
+    result = []
+    new_info = True
+    for dim_key, benchmark_group in groupby(data, grp_benchmark):
+        #Create folder if not exisits
+        folder = DATA_FOLDER + '/F' + str(dim_key[0])
+        try:
+            os.makedirs(folder)
+        except OSError:
+            pass
+
+        indexfile = DATA_FOLDER + '%s.info' % ( '/F' + str(dim_key[0]))
+
+        # Create files
+        filename = '%s-%02d_f%s_DIM%d' % (str(experiment_id), 0,
+                                          str(dim_key[0]), dim_key[1])
+        datafile =  folder+'/' + filename + '.tdat'
+        hdatafile = folder+'/' + filename + '.dat'
+
+        print ("F" + str(dim_key[0]) + " Dimension:" + str(dim_key[1]))
+
+        comment = "% SwarmEA: Message Based Algorithm see info in root folder"
+        if new_info:
+            info = """funcId = %s, DIM = %d, Precision = 1.000e-08, algId = 'SwarmEA'\n%s\nF%s/%s.dat, """ % \
+               (str(dim_key[0]), dim_key[1], comment, dim_key[0],filename)
+            new_info = False
+        else:
+            info = """\nfuncId = %s, DIM = %d, Precision = 1.000e-08, algId = 'SwarmEA'\n%s\nF%s/%s.dat, """ % \
+                   (str(dim_key[0]), dim_key[1], comment, dim_key[0], filename)
+
+
+        f = open(indexfile, 'a')
+        f.writelines(info)
+
+        f.close()
+        new_instance = True
+
+        for instance_key,benchmark in groupby(benchmark_group, grp_instance):
+            print  (" Instance:" + str(instance_key[1]))
+
+
+            coco = CoCoData(dim_key[1], function= dim_key[0], instance= instance_key[1] )
+            index = 0
+            total = 0
+            buffr = []
+            hbuffr =[]
+
+            for row in benchmark:
+                data_row = []
+                row_id=0
+                for e in row['evals']:
+                    # We have to change this to a more practical solution
+
+                    num_evals = row['params']['sample_size']
+                    
+
+                    if len(e) >= 4:
+                        num_evals = e['num_of_evals']
+
+
+                    data_row.append((e['best_fitness'], row['algorithm'], e['gen_num'],num_evals, e['best_fitness'],row['fopt'], '%+10.9e'% ( e['best_fitness']-row['fopt']),e[ 'best_solution']))
+                    row_id+=1
+                data_row.sort(reverse=True)
+                for r in data_row:
+                    coco.evalfun(*r[1:],buffr=buffr,hbuffr=hbuffr)
+
+            if buffr:
+                f = open(datafile, 'a')
+                f.write('%% function evaluation | noise-free fitness - Fopt'
+                        ' (%13.12e) | best noise-free fitness - Fopt | measured '
+                        'fitness | best measured fitness | x1 | x2...\n' % row['fopt']
+                        )
+
+
+                f.writelines(buffr)
+
+                f.close()
+            if hbuffr:
+                f = open(hdatafile, 'a')
+                f.write('%% function evaluation | noise-free fitness - Fopt'
+                        ' (%13.12e) | best noise-free fitness - Fopt | measured '
+                        'fitness | best measured fitness | x1 | x2...\n' % row['fopt']
+                        )
+                f.writelines(hbuffr)
+                f.close()
+
+
+            last = buffr[-1].split(" ")
+
+
+
+
+
+
+            f = open(indexfile, 'a')
+            if new_instance:
+                f.write('%s:%d|%.1e' % (instance_key[1],int(last[0]), float(last[1])))
+                new_instance = False
+            else:
+                f.write(', %s:%d|%.1e' % (instance_key[1], int(last[0]), float(last[1])))
+
+            f.close()
+    return DATA_FOLDER
