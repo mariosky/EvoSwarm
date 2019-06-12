23a98240008d160a9b85e5e8f5cf28d6a33b8953
JJ Merelo <jjmerelo@gmail.com>
2019-06-12 11:56:26 +0200

    Exports configuration

--- run_experiment.py
+++ run_experiment.py
@@ -1,27 +1,16 @@
 import redis
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+import sys
+import json
 
-y = """
-{
-"DIM_CONFIGURATION" : 
-    {
-        "2" : { "NGEN":50, "POP_SIZE": 100, "MAX_ITERATIONS":20, "MESSAGES_PSO":0, "MESSAGES_GA":6 },
-        "3" : { "NGEN":50, "POP_SIZE": 100, "MAX_ITERATIONS":30, "MESSAGES_PSO":0, "MESSAGES_GA":2 },
-        "5": { "NGEN":50, "POP_SIZE": 100, "MAX_ITERATIONS":25, "MESSAGES_PSO":0, "MESSAGES_GA":4 },
-        "10":{ "NGEN":50, "POP_SIZE": 100, "MAX_ITERATIONS":25, "MESSAGES_PSO":0, "MESSAGES_GA":8 },
-        "20":{ "NGEN":50, "POP_SIZE": 200, "MAX_ITERATIONS":25, "MESSAGES_PSO":0, "MESSAGES_GA":8 },
-        "40":{ "NGEN":50, "POP_SIZE": 200, "MAX_ITERATIONS":25, "MESSAGES_PSO":0, "MESSAGES_GA":16 }
-    },
-    "EXPERIMENT_ID" : "314152",
-    "GA_WORKER_RATIO" : 0.5,
+r = redis.Redis(host='localhost', port=6379, db=0)
 
-    "FUNCTIONS": [4],
-    "DIMENSIONS" : [10],
-    "INSTANCES" : [1,2,3],  
-    "CXPB_RND": [0.2, 0.6],
-    "MUTPB_RND": [0.1, 0.3]
-}
-"""
+if len(sys.argv) > 1 :
+    configuration =  sys.argv[1]
+else:
+    configuration = "default_conf.json"
 
-r.rpush("setup_queue", y)
+with open(configuration,"r") as conf:
+    configuration_data = json.load(conf)
+
+r.rpush("setup_queue", configuration_data)
