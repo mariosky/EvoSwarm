082dc4eeb0f1d49a9b544dcd60b3f92c2ea78c81
JJ Merelo <jjmerelo@gmail.com>
2019-06-13 19:52:28 +0200

    Changes instances and conf file

--- run_experiment.py
+++ run_experiment.py
@@ -16,6 +16,10 @@ with open(configuration,"r") as conf:
     configuration_data = json.load(conf)
 
 configuration_data['EXPERIMENT_ID'] = str(id)
+configuration_data['INSTANCES'] = []
+for i in range(15):
+    configuration_data['INSTANCES'].append(1)
+    
 print(configuration_data)
 
 r.rpush("setup_queue", json.dumps(configuration_data))
