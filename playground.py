c4288ae81af1265219f71829ce444a574d452d70
mariosky <mariosky@gmail.com>
2019-06-27 22:07:05 -0700

    ts boxplot

--- playground.py
+++ playground.py
@@ -2,6 +2,7 @@
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
 from datetime import datetime
 from itertools import groupby
@@ -16,24 +17,74 @@ result = []
 new_info = True
 data = None
 #with open(r'experiment_data\1561177738\swarm_ea_1561177738.json') as json_file:  
-with open(r'experiment_data\1561178365\swarm_ea_1561178365.json') as json_file:      
-    data = json.load(json_file)
-    for d in data:
-        print( d["benchmark"] , d["dim"], d["instance"])
+#with open(r'experiment_data\1561185389\swarm_ea_1561185389.json') as json_file:      
+#    data = json.load(json_file)
+#    for d in data:
+#        print( d["benchmark"] , d["dim"], d["instance"])
 
 #print(data[0])
 
 
-for dim_key, benchmark_group in groupby(data, grp_benchmark):
-    print(dim_key, benchmark_group)
+#for dim_key, benchmark_group in groupby(data, grp_benchmark):
+#    print(dim_key, benchmark_group)
+with open(r'D:\exp_data\1561526317_1w\swarm_ea_1561526317.json') as json_file: 
+#with open(r'experiment_data\1561185389\swarm_ea_1561185389.json') as json_file:  
+    data = json.load(json_file)
+    #print(data[0])
+    for p in data:
+       dt_object = datetime.fromtimestamp(p['time_stamp'])
+       #print(p["dim"], p["instance"])
+       messages.append((pd.to_datetime(dt_object), 1 , p["dim"], p["instance"],  p["best_score"],  p["fopt"]))
 
-#with open('swarm.json') as json_file:  
-#    data = json.load(json_file)
-#    for p in data:
-        #dt_object = datetime.fromtimestamp(p['time_stamp'])
-        #print(p["dim"], p["instance"])
-        #messages.append((pd.to_datetime(dt_object), 1 , p["dim"], p["instance"],  p["best_score"],  p["fopt"]))
+labels = ['ts','n','dim','instance','best_score','fopt']
+sns.set(style="white", palette="muted", color_codes=True)
 
+f, axes = plt.subplots(1, 6, sharex=True, sharey=True)
+
+#sns.despine(left=True)
+
+
+
+
+
+df = pd.DataFrame.from_records(messages, columns=labels)
+#df.set_index('ts', drop=False, inplace=True)
+#group_by_dim = df.groupby("dim")
+
+#for name, group in group_by_dim:
+#        #print(group.n.resample('120S').sum())
+#        print(group.agg( {'ts':[np.min, np.max]} ))
+        
+#print(group_by_dim.agg( {'ts':[np.min, np.max]} ))
+#df2 = group_by_dim.resample('60S').agg( {'ts':[np.min, np.max] , 'n':np.sum })
+
+
+
+resample_step = {2:'20S', 3:'20S',5:'20S',10:'60S',20:'120S',40:'240S' }
+
+#for name, group in df2:
+#        print (df2.get_group(group))
+#        print (name, group)
+        #sns.barplot(x=group.ts, y=group.n.sum, palette="rocket")
+for i, (name, group) in enumerate (df.groupby("dim")):
+        group.set_index('ts', drop=False, inplace=True)
+        print(name)
+        resample = group.resample(resample_step[name]).agg( {'ts':[np.min, np.max] , 'n':np.sum })
+        sns.barplot(x=resample.ts.amax, y=resample.n['sum'], color="b", ax=axes[i])
+#for i, (name, group) in enumerate (df2.reset_index().groupby('dim')):
+        #sns.barplot(x=list(group.ts.amax), y=list(group.n.sum), palette="rocket")
+        #print([i for i in group.n['sum']] , list( range(1, len(group.n['sum']) + 1 )))
+        print(resample)
+        #for i in group.ts.amax:
+        #        print (i)
+        #sns.barplot(x=group.ts.amax, y=group.n['sum'], palette="rocket", ax=axes[count])
+
+plt.setp(axes, xticks=[])
+#plt.tight_layout()
+plt.show()
+#print(df2.ts.amin)
+
+#df2.reset_index().plot(kind='bar', use_index=True, y='n')
 
 # from rx import Observable, Observer
 # from rx.subjects import Subject
--- timeseries.py
+++ timeseries.py
@@ -4,63 +4,60 @@
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
 from datetime import datetime
+from itertools import groupby
+from operator import itemgetter
 
 
 
-messages = []
+# 1, 2, 4, 8 workers
+file_list = [r'D:\exp_data\1561526317_1w\swarm_ea_1561526317.json', 
+             r'D:\exp_data\1561558363_2w\swarm_ea_1561558363.json',
+             r'D:\exp_data\1561521129_4w\swarm_ea_1561521129.json',
+             r'D:\exp_data\1561516494_8w\swarm_ea_1561516494.json']
 
-# These files are in each folder 
+def get_data_frame(file):
+    messages = []
+    with open(file) as json_file: 
+        data = json.load(json_file)
+        for p in data:
+            dt_object = datetime.fromtimestamp(p['time_stamp'])
+            messages.append((pd.to_datetime(dt_object), 1 , p["dim"], p["instance"],  p["best_score"],  p["fopt"]))
+    return pd.DataFrame.from_records(messages, columns=labels)
 
-w1 = 'swarm_ea_1561054780.json'
-w2 = 'swarm_ea_1561074987.json'
-w4 = 'swarm_ea_1561082965.json'
-w8 = 'swarm_ea_1561087340.json'
-with open(w1) as json_file:  
-    data = json.load(json_file)
-    for p in data:
-        dt_object = datetime.fromtimestamp(p['time_stamp'])
-        messages.append((pd.to_datetime(dt_object), 1 , p["dim"], p["instance"],  p["best_score"],  p["fopt"]))
+
 
 labels = ['ts','n','dim','instance','best_score','fopt']
+sns.set(style="darkgrid", palette="muted", color_codes=True)
 
-df = pd.DataFrame.from_records(messages, columns=labels)
-df.set_index('ts', drop=False, inplace=True)
+f, axes = plt.subplots(4, 6, sharex=True, sharey='col')
 
-df.n.resample('10S').sum().plot()
+#sns.despine(left=True)
+resample_step = {2:'2S', 3:'5S',5:'10S',10:'30S',20:'60S',40:'100S' }
+worker_labels = ["1w", "2w", "4w", "8w"]
+for worker_index , file in enumerate(file_list):
+    df = get_data_frame(file)
+    for dim_index, (name, group) in enumerate (df.groupby("dim")):
+            group.set_index('ts', drop=False, inplace=True)
+            resample = group.resample(resample_step[name]).agg( {'ts':[np.min, np.max] , 'n':np.sum })
+            ax = axes[worker_index, dim_index]
+            print(worker_labels[worker_index] ,name, resample )
+            print(len(resample))
+            sns.lineplot(x=list(range(len(resample))), y=resample.n['sum'], color="b", ax=ax)
+            if dim_index == 0:
+                ax.set_ylabel(worker_labels[worker_index] )
+            else:
+                ax.set_ylabel("")
+            
+            if worker_index == len(worker_labels)-1:
+                 ax.set_xlabel("{0} second sample".format(resample_step[name][:-1]) )
+            else:
+                ax.set_xlabel("")
+            
+            if worker_index == 0:
+                ax.set_title('{}D'.format(name))
 
-with open(w2) as json_file:  
-    data = json.load(json_file)
-    for p in data:
-        dt_object = datetime.fromtimestamp(p['time_stamp'])
-        messages.append((pd.to_datetime(dt_object), 1 , p["dim"], p["instance"],  p["best_score"],  p["fopt"]))
-
-df = pd.DataFrame.from_records(messages, columns=labels)
-df.set_index('ts', drop=False, inplace=True)
-
-df.n.resample('10S').sum().plot()
-
-with open(w4) as json_file:  
-    data = json.load(json_file)
-    for p in data:
-        dt_object = datetime.fromtimestamp(p['time_stamp'])
-        messages.append((pd.to_datetime(dt_object), 1 , p["dim"], p["instance"],  p["best_score"],  p["fopt"]))
-
-df = pd.DataFrame.from_records(messages, columns=labels)
-df.set_index('ts', drop=False, inplace=True)
-
-df.n.resample('10S').sum().plot()
-
-with open(w8) as json_file:  
-    data = json.load(json_file)
-    for p in data:
-        dt_object = datetime.fromtimestamp(p['time_stamp'])
-        messages.append((pd.to_datetime(dt_object), 1 , p["dim"], p["instance"],  p["best_score"],  p["fopt"]))
-
-df = pd.DataFrame.from_records(messages, columns=labels)
-df.set_index('ts', drop=False, inplace=True)
-
-df.n.resample('10S').sum().plot()
-
+plt.setp(axes)
 plt.show()
